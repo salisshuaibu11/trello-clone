e5c5931139b4f8f641fa255f35d68ecd98136319
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-05-07 00:34:23 +0100

    test the styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { AppContainer, CardContainer, ColumnContainer, ColumnTitle } from "./styles"
+
 export const App = () => {
-  return null
+  return <AppContainer>
+    <ColumnContainer>
+      <ColumnTitle>Todo:</ColumnTitle>
+      <CardContainer>First Item</CardContainer>
+      <CardContainer>Second Item</CardContainer>
+      <CardContainer>Third Item</CardContainer>
+    </ColumnContainer>
+  </AppContainer>
 }
 
