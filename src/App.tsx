b0e83e5d725b332ce0f0eb1772040b2f6a6e9012
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-05-24 10:54:16 +0100

    Get data from AppStateContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useAppState } from "./AppStateContext";
+
 import { AddNewItem } from "./AddNewItem"
 import { Column } from "./Column"
 import { AppContainer } from "./styles"
 
 export const App = () => {
-  return <AppContainer>
-      <Column text="Todo:"/>
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={() => console.log("Item created")}/>
-  </AppContainer>
+  const { state } = useAppState();
+
+  return (
+    <AppContainer>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i}/>
+      ))}
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
+    </AppContainer>
+  )
 }
 
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,19 +1,24 @@
+import { useAppState } from "./AppStateContext";
+
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 import { ColumnContainer, ColumnTitle } from "./styles";
 
 type ColumnProps = {
   text: string;
+  index: number;
 }
 
-export const Column = ({text}: ColumnProps) => {
+export const Column = ({text, index}: ColumnProps) => {
+  const { state } = useAppState();
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <Card text="Generate app scaffold" />
-      <Card text="Learn Typescript" />
-      <Card text="Begin to use static typing" />
-      <AddNewItem toggleButtonText="+ Add another card" onAdd={() => console.log("New item added")} dark/>
+      {state.lists[index].tasks.map(task => (
+        <Card text={task.text} key={task.id}/>
+      ))}
+      <AddNewItem toggleButtonText="+ Add another card" onAdd={console.log} dark/>
     </ColumnContainer>
   )
 }
