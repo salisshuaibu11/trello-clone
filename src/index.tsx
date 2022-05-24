18438e41222c01efdcb22d88c25da575ff55d4e6
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-05-24 10:43:49 +0100

    Create context

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import {App} from './App';
+import { AppStateProvider } from "./AppStateContext";
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
   </React.StrictMode>
 );
 
